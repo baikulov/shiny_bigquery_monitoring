703419f6ca91694641614a44667ffc82cb26185e
baikulov <baikulov.ant@gmail.com>
2022-01-20 03:50:38 +0400

    add variables

--- server.R
+++ server.R
@@ -8,14 +8,14 @@ library(RPostgreSQL)
 server <- function(input, output) {
   
   # авторизация в bq
-  bq_auth(path = 'otus_credentials.json')
+  bq_auth(path = Sys.getenv("BQ_CREDENTIALS"))
   # создаём подключение к БД для выгрузки данных через SQL
   con <- dbConnect(drv = bigquery(),
-                   project = "r-studio-280613",
-                   dataset = 'Logging')
+                   project = Sys.getenv("BQ_PROJECT"),
+                   dataset = Sys.getenv("BQ_DATASET"))
   
   # текст запроса
-  text <- paste0("SELECT * FROM `r-studio-280613.Logging.project_metadata`")
+  text <- paste0("SELECT billing_account_id, cost FROM `", Sys.getenv("BQ_PROJECT"),".", Sys.getenv("BQ_DATASET"),".", Sys.getenv("BQ_BILLING_TABLE"), "` LIMIT 100")
   
   # делаем запрос в БД
   query <-  DBI::dbGetQuery(conn = con, statement = text)
@@ -36,13 +36,13 @@ server <- function(input, output) {
   #list <- dbGetQuery(con2,"SELECT table_name FROM shiny.information_schema.tables")
   
   # пишем из BQ в POSTGRE
-  dbWriteTable(con2, "metadata", query, row.names=FALSE, append=TRUE)
+  dbWriteTable(con2, "metadata2", query, row.names=FALSE, append=TRUE)
   
   # читаем таблицу для отчета
-  metadata <- dbGetQuery(con2, "SELECT * FROM metadata")
+  metadata <- dbGetQuery(con2, "SELECT * FROM metadata2")
   
   output$plot1 <- renderPlot({
-    data <- metadata$row_count
+    data <- metadata$cost
     hist(data)
   })
 }
