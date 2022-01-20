06c5d6e04df4bc86038524161eba652f3d27c41c
baikulov <baikulov.ant@gmail.com>
2022-01-20 07:56:39 +0400

    tmp delete system mkdir

--- server.R
+++ server.R
@@ -16,7 +16,7 @@ server <- function(input, output) {
     
     req(input$file)
     
-    system("mkdir keys")
+    #system("mkdir keys")
     file.copy(input$file$datapath, "keys/key2.json", overwrite = TRUE)
     
     bq_auth(path = "keys/key2.json")
